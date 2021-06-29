e44089e89128be377375cc6b997f26dfe7e53cfe
Yedidyah Bar David <didi@redhat.com>
2021-06-29 11:37:58 +0300

    test, do not merge: debug, build base, local iso
    Change-Id: I9e5c24ac81aa2ef0984328360dd6ab2f010a4e9b
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- build_node.sh
+++ build_node.sh
@@ -12,9 +12,10 @@ export DISTRO=node
 export SPARSIFY_BASE=no
 export DISK_SIZE=80G
 export BUILD_UPGRADE=
+export BUILD_BASE=yes
 export BUILD_ENGINE_INSTALLED=
 export BUILD_HOST_INSTALLED=
 export BUILD_HE_INSTALLED=
-export INSTALL_URL=https://resources.ovirt.org/pub/ovirt-4.4/iso/ovirt-node-ng-installer/4.4.3-2020112920/el8/ovirt-node-ng-installer-4.4.3-2020112920.el8.iso
+export INSTALL_URL=/home/ybardavi/node/ovirt-node-ng-installer-4.4.6-2021051809.el8.iso
 
-make -e rpm
+make -d -e rpm
